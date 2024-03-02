e34e6c65febd0ac09f60962162ce5774b60eb49b
pprograminha <marcosproenca144@gmail.com>
2024-03-01 23:34:11 -0300

    revert: serializing json

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-import { withSuperjson } from 'next-superjson'
 const nextConfig = {}
 
-export default withSuperjson()(nextConfig)
+export default nextConfig
