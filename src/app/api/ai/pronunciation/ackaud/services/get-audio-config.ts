67c9cf90aae55253adcef6dde76687d1fdd3b6c3
pprograminha <marcosproenca144@gmail.com>
2024-03-02 12:40:39 -0300

    improve: audio config

--- src/app/api/ai/pronunciation/ackaud/services/get-audio-config.ts
+++ src/app/api/ai/pronunciation/ackaud/services/get-audio-config.ts
@@ -1,18 +1,7 @@
-import { env } from '@/env'
-import { openPushStream } from '@/lib/file-push-stream'
-import fs from 'fs'
 import * as SpeechSDK from 'microsoft-cognitiveservices-speech-sdk'
 
 export const getAudioConfig = async (file: File) => {
-  const bytes = await file.arrayBuffer()
-  const buffer = Buffer.from(bytes)
-  const filepath =
-    env.NODE_ENV === 'production' ? `/tmp/${file.name}` : `tmp/${file.name}`
-
-  await fs.promises.writeFile(filepath, buffer)
-
-  const audioStream = openPushStream(filepath)
-  const audioConfig = SpeechSDK.AudioConfig.fromStreamInput(audioStream)
+  const audioConfig = SpeechSDK.AudioConfig.fromWavFileInput(file)
 
   return audioConfig
 }
