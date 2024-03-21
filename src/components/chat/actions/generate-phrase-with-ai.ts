2c4fb59373dc8b170b01ff00b1a8df1f38277f79
pprograminha <marcosproenca144@gmail.com>
2024-03-21 11:21:58 -0300

    refactor: generate phrase

--- src/components/chat/actions/generate-phrase-with-ai.ts
+++ src/components/chat/actions/generate-phrase-with-ai.ts
@@ -12,14 +12,12 @@ export async function generatePhraseWithAi(): Promise<string | null> {
         role: 'system',
         content: `
         Você gerará somente uma frase em inglês.
-        Com base nas palavras abaixo você deve gerar uma frase optando por palavras que não foram listadas abaixo, porém não deve desconsiderar a opção de usá-las, caso usá-las opte pelas palavras com o "avgAccuracyScore" e "wordCount" mais baixo.
-        A frase deverá ser criada de forma aleatória considerando as condições acima.
         Quanto mais palavras abaixo maior será a frase.
         As vezes gere a frase como se fosse uma expressão.
-        Evite ao máximo criar a frase com as palavras abaixo (Isso é extremamente importante)!!!
+        Não crie a frase com base nas palavras abaixo (Isso é extremamente importante)!!!
 
     
-        Palavras:
+        Palavras que não devem ser usadas:
         ${(await getWordsList()).reduce((p, c) => `${p} | avgAccuracyScore: ${c.avgAccuracyScore}, word: ${c.word}, wordCount: ${c.wordCount}`, '')}
         `,
       },
