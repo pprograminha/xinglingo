13f14567b32980dbd3ace17fd94ce4bbe456e95d
pprograminha <marcosproenca144@gmail.com>
2024-07-14 20:36:21 -0300

    fix: tooltip and dialog to mobile

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -4,12 +4,64 @@ import * as React from 'react'
 import * as TooltipPrimitive from '@radix-ui/react-tooltip'
 
 import { cn } from '@/lib/utils'
+import { useBreakpoint } from '@/hooks/use-breakpoint'
+import { createContext } from 'react'
 
 const TooltipProvider = TooltipPrimitive.Provider
 
-const Tooltip = TooltipPrimitive.Root
+type TooltipTriggerContextType = {
+  open: boolean
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
 
-const TooltipTrigger = TooltipPrimitive.Trigger
+export const TooltipTriggerContext = createContext<TooltipTriggerContextType>({
+  open: false,
+  setOpen: () => {}, // eslint-disable-line
+})
+
+const Tooltip: React.FC<TooltipPrimitive.TooltipProps> = ({
+  children,
+  ...props
+}) => {
+  const [open, setOpen] = React.useState<boolean>(props.defaultOpen ?? false)
+
+  const isMd = useBreakpoint('md')
+
+  return (
+    <TooltipPrimitive.Root
+      delayDuration={isMd ? props.delayDuration : 0}
+      onOpenChange={(e) => {
+        setOpen(e)
+      }}
+      open={open}
+    >
+      <TooltipTriggerContext.Provider value={{ open, setOpen }}>
+        {children}
+      </TooltipTriggerContext.Provider>
+    </TooltipPrimitive.Root>
+  )
+}
+// eslint-disable-next-line react/display-name
+const TooltipTrigger = React.forwardRef<
+  React.ElementRef<typeof TooltipPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Trigger>
+>(({ children, ...props }, ref) => {
+  const isMd = useBreakpoint('md')
+  const { setOpen } = React.useContext(TooltipTriggerContext)
+
+  return (
+    <TooltipPrimitive.Trigger
+      ref={ref}
+      {...props}
+      onClick={(e) => {
+        !isMd && e.preventDefault()
+        setOpen(true)
+      }}
+    >
+      {children}
+    </TooltipPrimitive.Trigger>
+  )
+})
 
 const TooltipContent = React.forwardRef<
   React.ElementRef<typeof TooltipPrimitive.Content>,
@@ -19,7 +71,7 @@ const TooltipContent = React.forwardRef<
     ref={ref}
     sideOffset={sideOffset}
     className={cn(
-      'z-50 overflow-hidden rounded-md bg-zinc-900 px-3 py-1.5 text-xs text-zinc-50 animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 dark:bg-zinc-900',
+      'z-50 overflow-hidden rounded-md bg-zinc-900 p-4 w-[80vw] md:w-auto text-xs text-zinc-50 animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 dark:bg-zinc-900',
       className,
     )}
     {...props}
